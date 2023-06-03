c1aeb527a16f14673f07aa4f601e57b72bccb791
GOPIKAKB <gopikakrishnakb@gmail.com>
2023-06-03 15:37:05 +0530

    flutter 3/6/23

--- lib/color_changing.dart
+++ lib/color_changing.dart
@@ -9,14 +9,43 @@ class Colorpage extends StatefulWidget {
 }
 
 class _ColorpageState extends State<Colorpage> {
+  Color clr = Colors.red;
+
+  int a = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          a++;
+
+          if (a == 0) {
+
+            clr = Colors.black;
+          } else{
+            clr = Colors.blueAccent;
+          }
+
+          
+          setState(() {
+            clr;
+          });
+
+
+        },
+      ),
       appBar: AppBar(
         backgroundColor: Colors.black26,
         title: Text("Color changing page"),
       ),
-      body: Container(),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: clr,
+        ),
+      ),
     );
   }
 }
